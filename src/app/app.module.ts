b1ebba64aaa16ea8702ec2564dfec2c015a0ed86
Guillaume <guillaumolina@gmail.com>
2019-03-25 15:19:02 +0100

    First angular commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MyFirstComponent } from './my-first/my-first.component';
+import { AppareilComponent } from './appareil/appareil.component';
+
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MyFirstComponent,
+    AppareilComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
